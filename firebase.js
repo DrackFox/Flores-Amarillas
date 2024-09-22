286feec962e949cf7a61097c7c80af106a4fb5a5
Billy <bgarciaseminario@gmail.com>
2024-09-21 20:23:47 -0500

    flores

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-app.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyBDTrL9HDdP0C8kJ1lfc7o7MKisHVfEzKU",
+    authDomain: "flores-amar.firebaseapp.com",
+    projectId: "flores-amar",
+    storageBucket: "flores-amar.appspot.com",
+    messagingSenderId: "856094111677",
+    appId: "1:856094111677:web:cc19094923dca9e9ad44d4"
+  };
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
